c9990838fc113589fb22c57e2bc38957dd47cb32
Dania801 <ssansnnom07@gmail.com>
2017-09-22 13:57:40 +0300

    Move the content of the second home page from the jade file to the controller

--- app_server/controllers/home.js
+++ app_server/controllers/home.js
@@ -34,7 +34,55 @@ module.exports.songList = function(req , res){
 };
 
 module.exports.extendedSongList = function(req , res){
-  res.render('home2' , {title: 'Welcome to Tab Tracker'}) ;
+  res.render('home2' , {
+    title: 'Home',
+    username: 'Dania',
+    caption: 'Here you get a chance to explore the universe of music. Enjoy the available tabs, and add more songs to play and give others an apportunity to learn!',
+    nav: {
+      home: 'HOME',
+      about: 'ABOUT',
+      logout: 'Log out'
+    },
+    songs: [{
+      title: 'My Heart will go on',
+      artist: 'Celine Dion',
+      cover: '../images/celine.jpg'
+    },{
+      title: 'Yesterday',
+      artist: 'The Beatles',
+      cover: '../images/beatles.jpg'
+    },{
+      title: 'Getting into you',
+      artist: 'Reliant K',
+      cover: '../images/reliant.jpg'
+    },{
+      title: 'Classic car',
+      artist: 'Bright Eyes',
+      cover: '../images/bright.jpg'
+    }],
+    recentlyViewed: [{
+        title: 'Getting into you',
+        artist: 'Reliant K',
+        cover: '../images/reliant.jpg'
+      },{
+        title: 'Yesterday',
+        artist: 'The Beatles',
+        cover: '../images/beatles.jpg'
+      },{
+        title: 'Classic car',
+        artist: 'Bright Eyes',
+        cover: '../images/bright.jpg'
+      }],
+    bookmarekedSongs: [{
+      title: 'Classic car',
+      artist: 'Bright Eyes',
+      cover: '../images/bright.jpg'
+    },{
+      title: 'My Heart will go on',
+      artist: 'Celine Dion',
+      cover: '../images/celine.jpg'
+    }]
+  }) ;
 };
 
 module.exports.addSong = function(req , res){
