dffdff2bdd2a16eddc4f25568398e6ec46439c61
Dania801 <ssansnnom07@gmail.com>
2017-09-22 11:36:53 +0300

    Move the content of the first home page from the jade file to the controller

--- app_server/controllers/home.js
+++ app_server/controllers/home.js
@@ -1,5 +1,36 @@
 module.exports.songList = function(req , res){
-  res.render('home1' , {title: 'Welcome to Tab Tracker'}) ;
+  res.render('home1' , {
+    title: 'Welcome to Tab Tracker',
+    nav: {
+      home: 'HOME',
+      about: 'ABOUT',
+      login: 'Login'
+    },
+    cover: {
+      title: 'Tab Tracker',
+      desc: 'Keep Track of guitar tabs !'
+    },
+    featured: {
+      title: 'FEATURED SONGS',
+      songs: [{
+        title: 'My Heart will go on',
+        artist: 'Celine Dion',
+        cover: '../images/celine.jpg'
+      },{
+        title: 'Yesterday',
+        artist: 'The Beatles',
+        cover: '../images/beatles.jpg'
+      },{
+        title: 'Getting into you',
+        artist: 'Reliant K',
+        cover: '../images/reliant.jpg'
+      },{
+        title: 'Classic car',
+        artist: 'Bright Eyes',
+        cover: '../images/bright.jpg'
+      }]
+    }
+  }) ;
 };
 
 module.exports.extendedSongList = function(req , res){
