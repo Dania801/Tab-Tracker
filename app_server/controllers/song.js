0d54e01ae3774aad3e86788ce0028548eb97b423
Dania801 <ssansnnom07@gmail.com>
2017-10-08 17:06:54 +0300

    Finished rendering the second tab page

--- app_server/controllers/song.js
+++ app_server/controllers/song.js
@@ -36,30 +36,35 @@ module.exports.songTab = function(req , res){
 };
 
 
-var renderExtededSongTab = function(req, res){
+var renderExtededSongTab = function(req, res, body){
   res.render('tab2' , {
+    request: req.params.songid,
     title: 'Song Tabs',
     nav:{
       home: 'HOME',
       about: 'ABOUT',
       logout: 'Log out'
     },
-    songInfo: {
-      title: 'My heart will go on',
-      artist: 'Celine Dion',
-      album: "let's talk about love",
-      year: '1997',
-      genre: 'Pop',
-      cover: '../../images/celine.jpg'
-    },
-    songTab: '| e|----------------------------|-------5-------------0--------------7------| \n B|-------5---7----7---9-------|-----------9----------------7--------------| \n D|----------------------------|----------------------------7--------------| \n G|----------------------------|----------------9-----------6--------------| \n A|----------------------------|----------------------------0--------------| \n E|-----------------0----------|-------------------------------------------| \n \n e|-------5----4----0-------------|----------------------| \n B|-------------------------------|----------------------| \n G|------------------------4------|------------2---------| \n D|-------------------------------|----------------------| \n A|-------------------------------|------------0---------| \n E|-----------------5-------------|----------------------| \n \n',
-    songLyrics: "[Verse 1]\nEvery night in my dreams\nI see you, I feel you\nThat is how I know you go on\nFar across the distance\nAnd spaces between us\nYou have come to show you go on\n\n[Chorus]\nNear, far, wherever you are\nI believe that the heart does go on\nOnce more you open the door\nAnd you're here in my heart\nAnd my heart will go on and on\n\n",
-    youtube: ''
+    songInfo: body
   }) ;
 }
 
 module.exports.extendedSongTab = function(req , res){
-  renderExtededSongTab(req, res);
+  var requestOptions, path;
+  path = '/api/songs' ;
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: 'GET',
+    json: {},
+    qs: {}
+  }
+  request(requestOptions, function(err, response, body){
+    if(err){
+
+    }else{
+      renderExtededSongTab(req, res, body);
+    }
+  })
 };
 
 
